20e81e997359cdceb422edac830e0a8c349428c5
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2024-10-02 19:21:08 +0530

    updated contact us section

--- src/components/home/ContactUs.jsx
+++ src/components/home/ContactUs.jsx
@@ -1,6 +1,5 @@
 "use client";
-
-import { Box, Typography, Button, TextField } from "@mui/material";
+import { Box, Typography, Button, TextField, Stack } from "@mui/material";
 import { useState } from "react";
 
 const ContactUs = () => {
@@ -11,17 +10,17 @@ const ContactUs = () => {
     message: "",
   });
   const [submitted, setsubmitted] = useState(false);
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setContactForm((prevState) => ({
       ...prevState,
       [name]: value,
-    }));
+    })); // updates each input element in the form
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(e);
     if (
       contactForm.name.length > 0 &&
       contactForm.email.length > 0 &&
@@ -29,10 +28,9 @@ const ContactUs = () => {
       contactForm.message.length > 0
     ) {
       setsubmitted(true);
-
       try {
         const sendEmailRes = {
-          email: "testdavose@gmail.com",
+          email: "pizzashop@gmail.com",
           name: contactForm.name,
           Uemail: contactForm.email,
           company: contactForm.company,
@@ -46,156 +44,189 @@ const ContactUs = () => {
     }
   };
 
-  const contactFormsubmitMessage = {
-    minWidth: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    pb: 15,
-    px: 8,
-    mt: 0,
-    pt: 10,
-    gap: "80px",
-    bgcolor: "white",
-    textAlign: "center",
-  };
-
   return (
     <Box
-      component="section"
-      // sx= {{
-      //   display: "flex",
-      //   justifyContent: "space-between",
-      //   bgcolor: "white",
-      //   pb: 15,
-      //   px: 8,
-      //   mt: 0,
-      //   pt: 10,
-      // }}
-      // style={submitted ? { contactFormsubmitMessage } : {}}
-      sx={
-        submitted
-          ? contactFormsubmitMessage
-          : {
-              display: "flex",
-              justifyContent: "space-between",
-              gap: "25px",
-              flexDirection: {
-                xs: "column",
-                md: "row",
-              },
-              alignItems: {
-                xs: "start",
-                md: "start",
-              },
-
-              pb: 15,
-              px: {
-                xs: 3,
-                sm: 5,
-                md: 8,
-              },
-              mt: 0,
-              pt: 10,
-            }
-      }
+      sx={{
+        bgcolor: "white",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        py: {
+          xs: 5,
+          md: 10,
+        },
+      }}
     >
-      <Typography
-        color="#151d23"
-        sx={{
-          textAlign: !submitted && "center",
-          fontSize: {
-            xs: "34px",
-            sm: "40px",
-            md: "50px",
-          },
-        }}
-      >
-        Contact Us
-      </Typography>
-      {/* style={{width: "50%"}} */}
-
       {!submitted ? (
         <Box
-          component="form"
+          component="section"
           sx={{
             width: {
-              xs: "100%",
-              md: "45%",
+              xs: "90%",
+              md: "85%",
+              lg: "70%",
             },
+            display: "flex",
+            justifyContent: "space-between",
+            gap: "25px",
+            flexDirection: {
+              xs: "column",
+              md: "row",
+            },
+            alignItems: {
+              xs: "start",
+              md: "start",
+            },
+            px: {
+              xs: 3,
+              sm: 5,
+              md: 6,
+            },
+            py: 5,
+            bgcolor: "white",
+            border: {
+              xs: "none",
+              md: "1px solid #c4c4c4",
+            },
+            borderRadius: 2,
+            // filter: "drop-shadow(0 0 0.75rem #c5c5c5)",
           }}
         >
-          <TextField
-            fullWidth
-            label="Name"
-            margin="normal"
-            name="name"
-            value={contactForm.name}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Email"
-            margin="normal"
-            type="email"
-            name="email"
-            value={contactForm.email}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Company"
-            margin="normal"
-            name="company"
-            value={contactForm.company}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            label="Message"
-            margin="normal"
-            multiline
-            rows={4}
-            name="message"
-            value={contactForm.message}
-            onChange={handleChange}
-          />
-
-          <Button
-            variant="contained"
-            type="submit"
-            onClick={handleSubmit}
+          <Typography
+            color="#151d23"
             sx={{
-              mt: 2,
-              borderRadius: 1.5,
-              bgcolor: "#0e0d06",
-              color: "#fff",
-              width: "fit-content",
-              px: 3,
-              ":hover": {
-                bgcolor: "#0e0d06",
-                color: "#fff",
-                fontWeight: "600",
+              textAlign: !submitted && "center",
+              fontSize: {
+                xs: "34px",
+                sm: "40px",
+                md: "44px",
+                lg: "50px",
               },
             }}
           >
-            Send
-          </Button>
+            Contact Us
+          </Typography>
+          <Box
+            component="form"
+            sx={{
+              width: {
+                xs: "100%",
+                md: "45%",
+              },
+            }}
+          >
+            <TextField
+              fullWidth
+              label="Name"
+              margin="normal"
+              name="name"
+              value={contactForm.name}
+              onChange={handleChange}
+            />
+            <TextField
+              fullWidth
+              label="Email"
+              margin="normal"
+              type="email"
+              name="email"
+              value={contactForm.email}
+              onChange={handleChange}
+            />
+            <TextField
+              fullWidth
+              label="Company"
+              margin="normal"
+              name="company"
+              value={contactForm.company}
+              onChange={handleChange}
+            />
+            <TextField
+              fullWidth
+              label="Message"
+              margin="normal"
+              multiline
+              rows={4}
+              name="message"
+              value={contactForm.message}
+              onChange={handleChange}
+            />
+            <Button
+              variant="outlined"
+              type="submit"
+              onClick={handleSubmit}
+              sx={{
+                mt: 2,
+                color: "#fff",
+                borderColor: "#0e0d06",
+                bgcolor: "#0e0d06",
+                borderRadius: 1.5,
+                fontWeight: 500,
+                py: 1,
+                width: "fit-content",
+                fontSize: {
+                  md: "15px",
+                },
+                ":hover": {
+                  bgcolor: "#fff",
+                  borderColor: "#0e0d06",
+                  color: "#0e0d06",
+                },
+              }}
+            >
+              Send
+            </Button>
+          </Box>
         </Box>
       ) : (
         <Box
-          // width="45%"
-          // height={400}
-          textAlign={"center"}
-          alignContent={"center"}
+          component="section"
+          sx={{
+            width: "90%",
+            py: {
+              xs: 5,
+              md: 0,
+            },
+          }}
         >
-          <Typography variant="h3">We&apos;ll be in touch soon.</Typography>
-          <Typography>
-            You&apos;ll get an email confirmation for your call.
-          </Typography>
-          <Typography>
-            If you need more assistance, contact support@davose.com.
-          </Typography>
+          <Stack textAlign={"center"} alignContent={"center"} gap={3}>
+            <Typography
+              sx={{
+                fontSize: {
+                  xs: "32px",
+                  sm: "36px",
+                  md: "48px",
+                },
+              }}
+            >
+              We&apos;ll be in touch soon.
+            </Typography>
+            <Box>
+              <Typography
+                sx={{
+                  fontSize: {
+                    xs: "14px",
+                    sm: "16px",
+                    md: "18px",
+                    lg: "20px",
+                  },
+                }}
+              >
+                You&apos;ll get an email confirmation for your call.
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: {
+                    xs: "14px",
+                    sm: "16px",
+                    md: "18px",
+                  },
+                }}
+              >
+                If you need more assistance, contact support@pizza.com.
+              </Typography>
+            </Box>
+          </Stack>
         </Box>
       )}
     </Box>
